209cf7c3bf7f119136757888b27b81f341a13aad
Cristian <cristian.buciu89@gmail.com>
2021-01-08 02:06:16 +0100

    cart -> made the subtotal and made the cart to slide from right, created an overlay to cover the app while the cart is open and also clicking it will close the cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,22 @@ import HomeScreen from "./screens/home-screen/HomeScreen";
 import { Route } from "react-router-dom";
 import ProductScreen from "./screens/product-screen/ProductScreen";
 import Checkout from "./screens/checkout/Checkout.js";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleCartShow } from "./redux/cart/cart.actions.js";
 //!==================================================================
 function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.cart.showCart);
+  const overlayClickHandler = () => {
+    dispatch(toggleCartShow());
+  };
   return (
     <>
       <Header />
+      {showCart ? (
+        <div onClick={overlayClickHandler} className="overlay"></div>
+      ) : null}
+
       <main>
         <Route exact path="/shop" render={() => <HomeScreen />} />
         <Route path="/product/:id" component={ProductScreen} />
--- frontend/src/components/cart-dropdown/CartDropdown.js
+++ frontend/src/components/cart-dropdown/CartDropdown.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./CartDropdown.scss";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { TweenMax, Power3 } from "gsap";
+
 //!================================================
 const CartDropdown = () => {
   //!Function to calculate floatpoints accurate====
@@ -11,8 +13,30 @@ const CartDropdown = () => {
   //?==============================================
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cart.cartItems);
+  const cartItemsNumber = cartItems.reduce(
+    (accum, cartItem) => accum + cartItem.qty,
+    0
+  );
+  const subtotal = cartItems.reduce(
+    (accum, cartItem) => accum + cartItem.qty * cartItem.price,
+    0
+  );
+
+  const cartRef = useRef(null);
+  useEffect(() => {
+    TweenMax.to(cartRef.current, 0.8, {
+      opacity: 1,
+      x: 0,
+      ease: Power3.easeOut,
+    });
+  }, []);
   return (
-    <div className="cart">
+    <div ref={cartRef} className="cart">
+      <div className="cart__top">
+        <h3 className="cart__top--text">
+          You have {cartItemsNumber} items in your cart
+        </h3>
+      </div>
       {cartItems.map((item) => (
         <div className="cart__item">
           <div className="cart__item--image-container">
@@ -39,7 +63,10 @@ const CartDropdown = () => {
           </div>
         </div>
       ))}
-
+      <div className="cart__subtotal">
+        <h2 className="cart__subtotal--text">SUBTOTAL:</h2>
+        <h2 className="cart__subtotal--value">{roundToTwo(subtotal)}</h2>
+      </div>
       <div className="cart__checkout">
         <Link to="/checkout">
           <button className="cart__checkout--btn">Go to checkout</button>
