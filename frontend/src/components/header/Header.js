e4d0ccf8089b8ae22a34b75d6d5c7f056e32632e
Cristian <cristian.buciu89@gmail.com>
2021-01-11 16:55:26 +0100

    finished the login screen and made the logout functionality

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import { GiTechnoHeart } from "react-icons/gi";
 import { TiShoppingCart, TiUserOutline, TiHeart } from "react-icons/ti";
+import { IoMdArrowDropdown } from "react-icons/io";
 import { Link, NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import CartDropdown from "../cart-dropdown/CartDropdown.js";
 import { toggleCartShow } from "../../redux/cart/cart.actions.js";
+import UserDropdown from "../user-menu-dropdown/UserDropdown.js";
 //!==============================================================
 
 const Header = () => {
+  const [dropdownShow, setDropdownShow] = useState(false);
   const techTitle = "Cyber Heart";
   const dispatch = useDispatch();
   const cartToggle = useSelector((state) => state.cart.showCart);
   const cartItemsNumber = useSelector((state) =>
     state.cart.cartItems.reduce((accum, cartItem) => accum + cartItem.qty, 0)
   );
-
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
   return (
     <header className="header">
       <nav className="nav">
@@ -47,12 +51,35 @@ const Header = () => {
             <TiShoppingCart />
             <span className="nav__cart-items-number">{cartItemsNumber}</span>
           </div>
-
           {cartToggle ? <CartDropdown /> : null}
-          <Link className="nav__icon" to="/login">
-            {" "}
-            <TiUserOutline />
-          </Link>
+          {userInfo ? (
+            <>
+              {" "}
+              <span
+                className="nav__welcome-message"
+                onClick={() => setDropdownShow(!dropdownShow)}
+              >
+                Welcome,&nbsp;
+                <i>
+                  {" "}
+                  <strong>{userInfo.name}</strong>{" "}
+                </i>{" "}
+                &nbsp;
+                <IoMdArrowDropdown
+                  style={{ fontSize: "2rem" }}
+                  className={
+                    dropdownShow ? "dropdown-arrow-class" : "dropdown-revert"
+                  }
+                />
+              </span>
+              {dropdownShow ? <UserDropdown /> : null}
+            </>
+          ) : (
+            <Link className="nav__icon" to="/login">
+              {" "}
+              <TiUserOutline />
+            </Link>
+          )}
         </div>
       </nav>
     </header>
---
+++ frontend/src/components/user-menu-dropdown/UserDropdown.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./UserDropdown.scss";
+import { FiLogOut } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../redux/user/user.actions.js";
+//!==================================================
+const UserDropdown = () => {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+  return (
+    <div className="user-dropdown">
+      <ul>
+        <li className="user-dropdown__list-item">
+          <Link to="/profile">Profile</Link>
+        </li>
+
+        <li className="user-dropdown__list-item">My orders</li>
+        <hr />
+        <li
+          onClick={logoutHandler}
+          style={{
+            borderTop: "1px solid $color-grey-light-2",
+            paddingTop: ".5rem",
+          }}
+          className="user-dropdown__list-item"
+        >
+          Log Out&nbsp; <FiLogOut />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default UserDropdown;
