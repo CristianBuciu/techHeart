65d8bbb36a6a3723a0dc790ca98fac31223316d6
Cristian <cristian.buciu89@gmail.com>
2021-01-12 18:41:56 +0100

    created the profile model and implemented it to be created when a user is created so it can be updated and modified in the frontend part

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generate.token.js";
+import Profile from "../models/profile.model.js";
 
 //! DESCRIPTION : Auth user & get token
 //! ROUTE       :POST to /api/users/login
@@ -40,12 +41,14 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password,
   });
+  const profile = await Profile.create({ user });
   if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      profileId: profile._id,
       token: generateToken(user._id),
     });
   } else {
@@ -73,4 +76,47 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile };
+//! DESCRIPTION : Update user profile
+//! ROUTE       : PUT  /api/users/profile
+//! ACCESS      : PRIVATE
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  const profile = await Profile.findOne({ user: req.user._id });
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    profile.country = req.body.country || profile.country;
+    profile.line1 = req.body.line1 || profile.line1;
+    profile.line2 = req.body.line2 || profile.line2;
+    profile.city = req.body.city || profile.city;
+    profile.stateProvinceRegion =
+      req.body.stateProvinceRegion || profile.stateProvinceRegion;
+    profile.postalCode = req.body.postalCode || profile.postalCode;
+    profile.phoneNumber = req.body.phoneNumber || profile.phoneNumber;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    const updatedProfile = await profile.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      country: updatedProfile.country,
+      line1: updatedProfile.line1,
+      line2: updatedProfile.line2,
+      city: updatedProfile.city,
+      stateProvinceRegion: updatedProfile.stateProvinceRegion,
+      postalCode: updatedProfile.postalCode,
+      phoneNumber: updatedProfile.phoneNumber,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
---
+++ backend/models/profile.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const profileSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  country: {
+    type: String,
+  },
+  line1: {
+    type: String,
+  },
+  line2: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  stateProvinceRegion: {
+    type: String,
+  },
+  postalCode: {
+    type: String,
+  },
+  phoneNumber: {
+    type: String,
+  },
+});
+
+const Profile = mongoose.model("Profile", profileSchema);
+
+export default Profile;
