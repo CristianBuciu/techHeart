adae9428c85dbee412261bd4b7da6ebd40fcead0
CristianBuciu <cristian.buciu89@gmail.com>
2021-02-04 04:08:55 +0100

    1. made the backend endpoint for the update order to paid functionality

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -35,6 +35,29 @@ export const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
+//! DESCRIPTION : Update Order to paid
+//! ROUTE       : PUT /api/orders/:id/pay
+//! ACCESS      : PRIVATE
+export const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body._time,
+      email_address: req.body.payer.email_address,
+    };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
 //! DESCRIPTION : GET Order By ID
 //! ROUTE       : GET /api/orders/:id
 //! ACCESS      : PRIVATE
--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -5,11 +5,13 @@ const router = express.Router();
 import {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } from "../controllers/order.controller.js";
 
 router.route("/").post(protect, addOrderItems);
 
 //! Make sure this one is at the bottom /:id
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default router;
